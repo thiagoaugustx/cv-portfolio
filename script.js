a1772e5d8b49967ee6f68ce2976127829434fdc8
thiag <70293756+thiagoaugustx@users.noreply.github.com>
2025-04-13 02:02:56 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function toggleMenu() {
+  const nav = document.getElementById('navMobile');
+
+  if (!nav.classList.contains('active')) {
+    nav.classList.add('active');
+    nav.style.position = 'fixed';
+    nav.style.top = '0';
+    nav.style.left = '0';
+    nav.style.width = '100vw';
+    nav.style.height = '100vh';
+    nav.style.display = 'flex';
+    nav.style.flexDirection = 'column';
+    nav.style.justifyContent = 'center';
+    nav.style.alignItems = 'center';
+    nav.style.zIndex = '999';
+    nav.style.color = '#000';
+
+    // Adiciona o evento para cada link
+    const links = nav.querySelectorAll('a');
+    links.forEach(link => {
+      link.addEventListener('click', closeMenu);
+    });
+
+  } else {
+    closeMenu();
+  }
+}
+
+function closeMenu() {
+  const nav = document.getElementById('navMobile');
+  nav.classList.remove('active');
+  nav.style.display = 'none';
+}
